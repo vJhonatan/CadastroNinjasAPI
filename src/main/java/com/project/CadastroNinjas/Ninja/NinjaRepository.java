51fb14525f22965ea029a6ef1638d4c93780160e
Jhonatan <you@example.com>
2024-11-14 13:31:19 -0300

    feat: add ninja repository

---
+++ src/main/java/com/project/CadastroNinjas/Ninja/NinjaRepository.java
@@ -0,0 +1,6 @@
+package com.project.CadastroNinjas.Ninja;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+}
