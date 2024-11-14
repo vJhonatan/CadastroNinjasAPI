81671ef71d2d4aea2c434d775653afc26c17e1d5
Jhonatan <you@example.com>
2024-11-14 12:48:11 -0300

    initial commit

---
+++ src/main/java/com/project/CadastroNinjas/CadastroNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.project.CadastroNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CadastroNinjas/CadastroNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CadastroNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
