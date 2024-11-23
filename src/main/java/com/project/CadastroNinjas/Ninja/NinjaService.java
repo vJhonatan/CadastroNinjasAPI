0ba1acc71921c4187fa3c06f9df5133346199224
vJhonatan <jhonatan99653@gmail.com>
2024-11-23 20:16:47 -0300

    feat: add NinjaService

--- src/main/java/com/project/CadastroNinjas/Ninja/NinjaService.java
+++ src/main/java/com/project/CadastroNinjas/Ninja/NinjaService.java
@@ -4,10 +4,41 @@ import lombok.Data;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Data
 @Service
 public class NinjaService {
 
-    private final NinjaRepository ninjaRepository;
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    public NinjaModel newNinja(NinjaModel ninja){
+        return ninjaRepository.save(ninja);
+    }
+
+    public List<NinjaModel> getNinjas () {
+        return ninjaRepository.findAll();
+    }
+
+    public NinjaModel getNinjaById(Long id){
+        Optional<NinjaModel> ninja = ninjaRepository.findById(id);
+        return ninja.orElse(null);
+    }
+
+    public NinjaModel updateNinja(NinjaModel ninjaUpdated, Long id){
+        if (ninjaRepository.existsById(id)){
+            ninjaUpdated.setId(id);
+            return ninjaRepository.save(ninjaUpdated);
+        }
+
+        return null;
+    }
+
+    public void deleteNinja(Long id){
+        ninjaRepository.deleteById(id);
+    }
 }
